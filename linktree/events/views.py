74dbdfedaae0e1ebeed9350c74514e1c72f8390a
statescua <statescua@yahoo.com>
2022-06-14 13:39:08 -0400

    crud operations members

--- linktree/events/views.py
+++ linktree/events/views.py
@@ -3,8 +3,11 @@ from django.template import loader
 import random
 
 class Node:
+
     def __init__(self, value):
+
         self.data = value
+
         self.next = None
 
 def rev(arr):
@@ -14,18 +17,24 @@ def rev(arr):
     for i in arr:
 
         # if the node doesn t exist i mean is NULL, then do it   
+
         if head is None:
+
             head = Node(i)
+
         else:
+
             # insert the node at the end
             c = Node(i)
             
             ptr = head
+
             while ptr.next is not None:
+
                   ptr = ptr.next
+
             ptr.next = c  
             
-
     next = None
     prev = None 
     curr = head
@@ -41,15 +50,21 @@ def rev(arr):
     while c is not None:
         
         out.append(c.data)
+
         c = c.next
+
     return out
 
-def index(request):
+def index( request ):
 
   k = 0
-  arr = [0]*10 
-  for i in range(10):
-      arr[k] = random.randrange(0, 100)
+
+  arr = [ 0 ] * 10 
+
+  for i in range( 10 ):
+
+      arr [ k ] = random.randrange( 0, 100 )
+
       k += 1
 
   reverse = rev( arr )
