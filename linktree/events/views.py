2efafc02afe7b88c400bec3329844af6d7dc710f
nicaraguabreaza <nicaraguabreaza@hotmail.com>
2022-06-16 11:53:13 -0400

    longest increasing subsequence

--- linktree/events/views.py
+++ linktree/events/views.py
@@ -55,6 +55,54 @@ def rev(arr):
 
     return out
 
+def solveLIS( vec ):    
+    
+    n = len( vec )
+
+    L = [ 0 ] * ( n )
+
+    L[ n - 1 ] = 1
+
+    for i in range(n - 2, -1, -1):
+        
+        max = 0
+
+        for j in range(i + 1, n):
+
+            if vec[i] < vec[j] and L[j] > max:
+
+                max = L[ j ] 
+
+        L[i] = 1 + max
+
+    maxL = L[ 0 ]
+
+    startPos = 0
+
+    for i in range(1, n):
+
+        if L[ i ] > maxL:
+
+           maxL = L[ i ]
+
+           startPos = i
+
+    out = []
+
+    maxL2 = maxL
+
+    out.append(vec[startPos])       
+
+    for i in range(startPos + 1, n):
+
+        if vec[i] > vec[startPos] and maxL-1 == L[i]:
+
+            out.append(vec[i])
+            maxL -= 1       
+
+    return [maxL2, startPos, out]       
+
+
 def index( request ):
 
   k = 0
@@ -69,7 +117,26 @@ def index( request ):
 
   reverse = rev( arr )
 
-  context = {"arr" : arr, "reverse": reverse }
+  k = 0
+
+  list = [ 0 ] * 10 
+
+  for i in range( 10 ):
+
+      list [ k ] = random.randrange( 0, 100 )
+
+      k += 1
+
+  # Time Complexity O(n^2)
+  results = solveLIS( list )  
+
+  maxL = results[0]
+
+  startPos = results[1]
+
+  subsequence = results[2]
+
+  context = {"arr" : arr, "reverse": reverse, "list":list, "subsequence": subsequence, "startPos" : startPos, "maxL": maxL }
 
   template = loader.get_template('events.html')
 
